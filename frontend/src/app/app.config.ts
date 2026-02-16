2832e06be8518c5c7911c038ed67c8d73ff2b5de
DineniDaundasekara <daundasekaradineni@gmail.com>
2026-02-16 14:09:01 +0530

    add azure

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,17 +1,41 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
+import { MsalService, MsalGuard, MsalInterceptor, MsalBroadcastService, MSAL_INSTANCE, MSAL_GUARD_CONFIG, MSAL_INTERCEPTOR_CONFIG } from '@azure/msal-angular';
+import { InteractionType } from '@azure/msal-browser';
+import { createMsalInstance, loginRequest } from './auth-config';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptorsFromDi()),
     provideAnimations(),
     provideRouter(routes),
-    provideHttpClient(),
-    provideToastr()
+    provideToastr(),
+    { provide: MSAL_INSTANCE, useFactory: createMsalInstance },
+    MsalService,
+    MsalBroadcastService,
+    MsalGuard,
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useValue: {
+        interactionType: InteractionType.Redirect,
+        authRequest: loginRequest
+      }
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG, useValue: {
+        interactionType: InteractionType.Redirect,
+        protectedResourceMap: new Map([
+          // ['https://localhost:7251', ['api://eaea79e0-86c3-46ae-8c51-9d7c1c997ee2/access_as_user']],
+          // ['http://localhost:5043', ['api://eaea79e0-86c3-46ae-8c51-9d7c1c997ee2/access_as_user']]
+        ])
+      }
+    },
+    { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true }
   ]
 };
